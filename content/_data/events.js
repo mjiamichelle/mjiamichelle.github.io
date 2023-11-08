617515a23837d8f392af6a59ad2d7fb36530783c
Sean Yeh <109418+seanyeh@users.noreply.github.com>
2023-11-07 19:04:03 -0600

    Some updates

---
+++ content/_data/events.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    date: "2023-12-04 12:00",
+    title: "Studio Recital",
+    location: "Athens, GA",
+    time: "7:30pm",
+  },
+  {
+    date: "2024-01-22 12:00",
+    title: "Beethoven Violin Sonata Series at UGA",
+    location: "Athens, GA",
+    time: "7:30pm",
+  },
+  {
+    date: "2024-01-26 12:00",
+    title: "Solo Recital",
+    location: "Athens, GA",
+    time: "7:30pm",
+  }
+];
