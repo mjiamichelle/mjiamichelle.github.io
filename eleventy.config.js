545c6b8f41af51e8cf8a22a707dbc28b12b85a54
Sean Yeh <109418+seanyeh@users.noreply.github.com>
2025-06-16 21:01:07 -0500

    Add gha build

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,12 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("content/img");
   eleventyConfig.setBrowserSyncConfig({
-		files: './docs/css/**/*.css'
-	});
+    files: './_site/css/**/*.css'
+  });
   return {
     dir: {
       input: "content",
-      output: "docs"
+      output: "_site"
     }
   }
 };
